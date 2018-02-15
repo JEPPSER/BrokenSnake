de863bf6b9cbaa7c84ae98bd1156cde4cefead54
Jesper Bergström <jesper.b96@gmail.com>
2018-02-15 04:58:35 +0100

    Implemented Snake body

--- BrokenSnake.cpp
+++ BrokenSnake.cpp
@@ -1,5 +1,7 @@
 #include "BrokenEngine2D.hpp"
 #include <string>
+#include <thread>
+#include <chrono>
 
 class BrokenSnake : public BrokenEngine2D
 {
@@ -11,50 +13,64 @@ public:
 private:
 	struct Snake
 	{
-		float x;
-		float y;
+		int x;
+		int y;
 		float speed;
 		int direction;
+		std::vector<std::pair<int, int>> body;
 	};
 	Snake m_player;
+	float m_counter;
 
 protected:
 	virtual bool onCreate()
 	{
+		m_counter = 0;
 		m_player.x = 15;
 		m_player.y = 15;
 		m_player.speed = 5;
 		m_player.direction = 0;
+		m_player.body = { {m_player.x, m_player.y}, {m_player.x - 1, m_player.y}, {m_player.x - 2, m_player.y} };
 		return true;
 	}
 
 	virtual bool onUpdate(Input t_input, float t_elapsedTime)
 	{
-		// Player movement
-		if (m_player.direction == 0) {
-			m_player.x += t_elapsedTime * m_player.speed;
-		}
-		else if (m_player.direction == 1) {
-			m_player.y += t_elapsedTime * m_player.speed;
-		}
-		else if (m_player.direction == 2) {
-			m_player.x -= t_elapsedTime * m_player.speed;
-		}
-		else if (m_player.direction == 3) {
-			m_player.y -= t_elapsedTime * m_player.speed;
+		if (m_counter > 1) {
+			// Player movement
+			if (m_player.direction == 0) {
+				m_player.x += 1;
+			}
+			else if (m_player.direction == 1) {
+				m_player.y += 1;
+			}
+			else if (m_player.direction == 2) {
+				m_player.x -= 1;
+			}
+			else if (m_player.direction == 3) {
+				m_player.y -= 1;
+			}
+
+			for (int i = m_player.body.size() - 1; i > 0; i--) {
+				m_player.body[i] = m_player.body[i - 1];
+			}
+			m_player.body[0] = { m_player.x, m_player.y };
+			m_counter = 0;
 		}
 
+		m_counter += t_elapsedTime * m_player.speed;
+
 		// Player direction change
-		if (t_input.getKey('W').m_pressed) {
+		if (t_input.getKey('W').m_pressed && m_player.direction != 1) {
 			m_player.direction = 3;
 		}
-		else if (t_input.getKey('S').m_pressed) {
+		else if (t_input.getKey('S').m_pressed && m_player.direction != 3) {
 			m_player.direction = 1;
 		}
-		else if (t_input.getKey('A').m_pressed) {
+		else if (t_input.getKey('A').m_pressed && m_player.direction != 0) {
 			m_player.direction = 2;
 		}
-		else if (t_input.getKey('D').m_pressed) {
+		else if (t_input.getKey('D').m_pressed && m_player.direction != 2) {
 			m_player.direction = 0;
 		}
 
@@ -64,7 +80,9 @@ protected:
 	virtual bool onRender(Graphics t_graphics)
 	{
 		t_graphics.Clear();
-		t_graphics.Draw(m_player.x, m_player.y);
+		for (int i = 0; i < m_player.body.size(); i++) {
+			t_graphics.Draw(m_player.body[i].first, m_player.body[i].second);
+		}
 		return true;
 	}
 };
